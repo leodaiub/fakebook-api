07f989161ec48d7f378a8004536f7bfb5c5df7a9
Leonardo <leodaiub@gmail.com>
2020-06-02 11:14:00 -0300

    creating comments feature

---
+++ app/Controllers/Http/CommentController.js
@@ -0,0 +1,104 @@
+"use strict";
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Comment = use("App/Models/Comment");
+
+/**
+ * Resourceful controller for interacting with comments
+ */
+class CommentController {
+  /**
+   * Show a list of all comments.
+   * GET comments
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request, response, view }) {
+    const comments = Comment.query()
+      .orderBy("created_at", "desc")
+      .with("user")
+      .fetch();
+    return comments;
+  }
+
+  /**
+   * Render a form to be used for creating a new comment.
+   * GET comments/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create({ request, response, view }) {}
+
+  /**
+   * Create/save a new comment.
+   * POST comments
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response, auth }) {
+    const { id } = auth.user;
+    const data = request.only(["content", "id"]);
+
+    const comment = await Comment.create({
+      post_id: data.id,
+      user_id: id,
+      content: data.content,
+    });
+    await comment.load("user");
+    return comment;
+  }
+
+  /**
+   * Display a single comment.
+   * GET comments/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {}
+
+  /**
+   * Render a form to update an existing comment.
+   * GET comments/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit({ params, request, response, view }) {}
+
+  /**
+   * Update comment details.
+   * PUT or PATCH comments/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request, response }) {}
+
+  /**
+   * Delete a comment with id.
+   * DELETE comments/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, request, response }) {}
+}
+
+module.exports = CommentController;
